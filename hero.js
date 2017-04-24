0acbb669768757da975a82299e1cb7c2c81c1b8f
aaug <aaug@caasco.ca>
2017-04-24 14:06:08 -0400

    code cleanup

--- hero.js
+++ hero.js
@@ -2,64 +2,66 @@ window.projectNameHere = window.projectNameHere || {};
 
 projectNameHere = {
 
-    init : function () {
+  init: function() {
 
-        projectNameHere.classBannerRoot();
-        projectNameHere.bannerBGstyling();
+    projectNameHere.classBannerRoot();
+    projectNameHere.bannerBGstyling();
 
-    },
+  },
 
 
-    //USED TO APPEND "-hp" TO .mid PARENT FOR ABSTRACT BACKGROUND CONTROL
-    classBannerRoot: function () {
-      var bannerNameRoot = "mid-class-name-here";
-      var homeBanner = bannerNameRoot + "-hp";
-      var mainSlide = $('.' + homeBanner).parent().parent();
-      var bannerNameRoot__parent = $('.' + bannerNameRoot).parent();
+  //USED TO APPEND "-hp" TO .mid PARENT FOR ABSTRACT BACKGROUND CONTROL
+  classBannerRoot: function() {
+    var bannerNameRoot = "mid-class-name-here";
+    var homeBanner = bannerNameRoot + "-hp";
+    var mainSlide = $('.' + homeBanner).parent().parent();
+    var bannerNameRoot__parent = $('.' + bannerNameRoot).parent();
 
-      bannerNameRoot__parent.addClass(homeBanner + '-bg');
-    },
+    bannerNameRoot__parent.addClass(homeBanner + '-bg');
+  },
 
-    //USED TO CONTROL THE BG POSITION OF ABSTRACT BACKGROUND
-    bannerBGstyling : function () {
-      $('.mid-class-name-here-hp-bg')
-        .css("background-position", "71% center");
+  //USED TO CONTROL THE BG POSITION OF ABSTRACT BACKGROUND
+  bannerBGstyling: function() {
+    $('.mid-class-name-here-hp-bg')
+      .css("background-position", "71% center");
 
-      var rtime;
-      var timeout = false;
-      var delta = 200;
-      $(window).resize(function() {
-        rtime = new Date();
-        if (timeout === false) {
-          timeout = true;
-          setTimeout(resizeend, delta);
-        }
-      });
+    var rtime;
+    var timeout = false;
+    var delta = 200;
+    $(window).resize(function() {
+      rtime = new Date();
+      if (timeout === false) {
+        timeout = true;
+        setTimeout(resizeend, delta);
+      }
+    });
 
-      function resizeend() {
-        if (new Date() - rtime < delta) {
-          setTimeout(resizeend, delta);
+    function resizeend() {
+      if (new Date() - rtime < delta) {
+        setTimeout(resizeend, delta);
+      } else {
+        timeout = false;
+
+        var heroWith = $(window).width();
+
+        if (heroWith <= 500) {
+          console.log(heroWith);
+
+          $('.mid-class-name-here-hp-bg')
+            .css("background-position", "71% center");
+
         } else {
-          timeout = false;
-
-          var heroWith = $(window).width();
-
-          if (heroWith <= 500) {
-            console.log(heroWith);
-
-            $('.mid-class-name-here-hp-bg')
-              .css("background-position", "71% center");
-
-          } else {
-            $('.mid-class-name-here-hp-bg')
-              .css("background-position", "center");
-          }
+          $('.mid-class-name-here-hp-bg')
+            .css("background-position", "center");
         }
       }
     }
+  }
 
 };
 
-$(document).ready(function() { projectNameHere.init(); });
+$(document).ready(function() {
+  projectNameHere.init();
+});
 
 // ..............................................................................
